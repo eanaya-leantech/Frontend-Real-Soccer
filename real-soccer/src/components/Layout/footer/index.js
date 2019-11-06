cb993de14d284423afc0384de3925b7996afe47b
Andrés Marrugo <amarrugo@lean-tech.io>
2019-11-06 14:26:50 -0500

    brief fixes realized

--- real-soccer/src/components/Layout/footer/index.js
+++ real-soccer/src/components/Layout/footer/index.js
@@ -8,7 +8,7 @@ import './styles.scss';
 const Footer = () => {
     return (
         <footer className="footer">
-            <Text component={'span'}><StateIndicator currentState="active" />V 1.0 ...</Text>
+            <Text component={'span'}><StateIndicator currentState="active" />V 1.0</Text>
             <ImageNotDraggable image={'LOGO_LEAN_TECH'} width={'150px'}/>
         </footer>
     )
